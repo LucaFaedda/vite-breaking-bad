91158b82b3d1c387e3036e46238fb5e1d2e7bbdd
LucaFaedda <lucakarl91@gmail.com>
2023-01-13 15:39:08 +0100

    provato boostrap

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppListCard from './components/AppListCard.vue';
+import axios from 'axios';
+
+import {store} from './store.js'
     export default {
       components:{
         AppHeader,
         AppListCard
-      }
+      },
+      data() {
+        return {
+          listaCard: [],
+          store
+        }
+      },
+      created(){
+        this.eseguiListaCarte()
+
+      },
+      methods: {
+        eseguiListaCarte(){
+          axios.get(store.url).then((response) =>{
+            console.log(response.data.data)
+            
+          })
+        }
+      },
       
     }
 </script>   
 <template lang="">
   <div>
-    
+    <AppHeader message="Yu-Gi-Oh API"></AppHeader>
   </div>
 </template>
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+        export default {
+            props:{
+                message: String
+            }
+        }
+</script>
+<template lang="">
+    <div>
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-12 mt-5 text-center">
+                    <h1>{{message}}</h1>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</template>
+<style lang="scss">
+    
+</style>
